4c5ea63dc7d857f9f33acbc9b202ec4d2c1adb52
Isaac de Lyra Junior <isaac.lyra.junior@gmail.com>
2022-10-02 00:30:38 -0300

    Product

---
+++ src/main/java/com/ufrn/springproject/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.ufrn.springproject.services;
+
+import com.ufrn.springproject.entities.Product;
+import com.ufrn.springproject.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+}
