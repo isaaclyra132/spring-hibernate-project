d8328fe050df999243182f251966b3f4e2b67089
Isaac de Lyra Junior <isaac.lyra.junior@gmail.com>
2022-10-02 00:00:11 -0300

    Category

---
+++ src/main/java/com/ufrn/springproject/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.ufrn.springproject.repositories;
+
+import com.ufrn.springproject.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
